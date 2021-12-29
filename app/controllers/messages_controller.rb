d6362e0f2f83d793c5ce50cf1ef4536537a601f5
xcattleyax <novels.satellite@gmail.com>
2021-12-29 15:24:15 +0900

    メッセージ投稿フォームの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,13 @@
 class MessagesController < ApplicationController
 
   def index
+    if user_signed_in?
+      @groups = current_user.group
+    end
+    @message = Message.new
   end
 
+  def create
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'tops#index'
   resources :groups, only:[:new, :create] do
-    resources :messages, only:[:index]
+    resources :messages, only:[:index, :create]
   end
 
 end
