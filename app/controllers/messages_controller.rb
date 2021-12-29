e09c14433933ffed4c1ae91bab13da2b352712f9
xcattleyax <novels.satellite@gmail.com>
2021-12-29 15:50:27 +0900

    メッセージ一覧を表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,23 @@
 class MessagesController < ApplicationController
 
   def index
-    if user_signed_in?
-      @groups = current_user.group
-    end
+    @group = Group.find(params[:group_id])
+    @groups = current_user.group
     @message = Message.new
+    @messages = Message.where(group_id: params[:group_id])
   end
 
   def create
+    @message = Message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(group_id: params[:group_id])
+    else
+      render group_messages_path(group_id: params[:group_id])
+    end
   end
-  
+
+  private
+  def message_params
+    params.require(:message).permit(:message).merge(user_id: current_user.id, group_id: params[:group_id])
+  end
 end
