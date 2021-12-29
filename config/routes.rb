1553b8f09fc1febaff2705be81a9aa43a6aa5225
xcattleyax <novels.satellite@gmail.com>
2021-12-29 13:40:36 +0900

    チャットルームへの遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: 'tops#index'
-  resources :groups, only:[:new, :create]
+  resources :groups, only:[:new, :create] do
+    resources :messages, only:[:index]
+  end
 
 end
