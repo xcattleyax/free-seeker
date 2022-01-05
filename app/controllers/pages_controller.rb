89ed224b7422c5aa555ac5b7f0152d0f8dee100a
xcattleyax <novels.satellite@gmail.com>
2022-01-05 11:44:06 +0900

    ページ編集機能実装

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -16,6 +16,19 @@ class PagesController < ApplicationController
     end
   end
 
+  def edit
+    @page = Page.find(params[:id])
+    @post = Post.find(params[:post_id])
+    @pages = @post.pages
+    @page_length = @page.page
+  end
+
+  def update
+    page = Page.find(params[:id])
+    page.update(page_params)
+    redirect_to new_post_page_path(params[:post_id])
+  end
+
   private
   def page_params
     params.require(:page).permit(:content, :text, :page).merge(post_id: params[:post_id])
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     end
   end
   resources :posts, only:[:new, :create, :edit, :update] do
-    resources :pages, only:[:new, :create]
+    resources :pages, only:[:new, :create, :edit, :update]
   end
 
 end
