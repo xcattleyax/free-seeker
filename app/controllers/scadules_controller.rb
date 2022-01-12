20e1c1f1326b066a1991fd4de500dd18fce7b5fb
xcattleyax <novels.satellite@gmail.com>
2022-01-12 14:35:49 +0900

    スケジュール新規作成機能実装

--- app/controllers/scadules_controller.rb
+++ app/controllers/scadules_controller.rb
@@ -11,4 +11,23 @@ class ScadulesController < ApplicationController
     @scadules = @group.scadules
   end
 
+  def new
+    @scadule = Scadule.new
+    @groups = current_user.groups
+  end
+
+  def create
+    @scadule = Scadule.new(scadule_params)
+    if @scadule.save
+      redirect_to scadules_path
+    else
+      @groups = current_user.groups
+      render "new"
+    end
+  end
+
+  private
+  def scadule_params
+    params.require(:scadule).permit(:date, :content, :text, :group_id)
+  end
 end
