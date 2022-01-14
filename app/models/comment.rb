64d2524ea61ae0661d6646f77164a50e2b8363b6
xcattleyax <novels.satellite@gmail.com>
2022-01-14 17:55:53 +0900

    コメント一覧ページ実装

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
 
-  with options presence: true do
+  with_options presence: true do
     validates :comment
     validates :status_id
   end
